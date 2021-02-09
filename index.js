7e9ac2df4855af5aeaf4a402eca708c238fcd28f
ailedviv <ailedviv20@hotmail.com>
2021-02-09 13:57:05 -0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+const express = require('express')
+const path = require('path')
 const http = require('http');
 
 const hostname = '0.0.0.0';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -12,3 +14,15 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 }); 
+
+/*
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))*/
