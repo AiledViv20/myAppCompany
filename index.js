84e44e9cec2f981698e466f0c0e2378837f0070b
ailedviv <ailedviv20@hotmail.com>
2021-02-09 13:29:45 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-//const http = require('http');
+const http = require('http');
 app.use(express.static('public'));
 
 const hostname = '0.0.0.0';
@@ -9,7 +9,6 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  app.use(express.static('public'))
 });
 
 server.listen(port, hostname, () => {
